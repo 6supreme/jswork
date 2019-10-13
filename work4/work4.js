250e773cf4dc26adb383e168f424533606c92ee8
jyf <2234353342@qq.com>
2019-10-13 11:00:38 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -4,21 +4,22 @@ level = parseFloat(level)&&Number(level)
 if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
-for(let i=1;i<=level;++1){
+for(let i=1;i<=level;++i){
     let blank = level -i;
     for (let k=0;k<blank;++k){
-        document.write('&nbsp');
+        document.write('&nbsp;');
+   }
+   let star = i * 2 - 1
+   for (let j=0;j<star;++j){
+        document.write('*')
+   }
+    document.write("<br>")
 }
-let star = i * 2 - 1
-for (let j=0;j<star;++j){
-    document.write('*')
-}
-document.write("<br>")
-}
+
 let str = '<table border="1">'
-for (let i=1;i<10;++j){
+for (let i=1;i<10;++i){
     str += '<tr>'
-    for (var j=1;j<=1;++j){
+    for (var j=1;j<=i;++j){
         str +='<td>' + j + '*' + i + '=' + ( j * j ) + '</td>'
     }
     str += '</tr>'
