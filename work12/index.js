767b869d1a8ba40b55e20978a760152c1326719d
jyf <2234353342@qq.com>
2019-11-30 16:18:21 +0800

    work13

--- work12/index.js
+++ work12/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 let year = new Date().getFullYear()
 document.getElementById("cal").innerHTML = calendar(year)
 function setCalendar(year) {
 
 } 
-=======
-let year = new Data().getFullYear()
-document.getElementById("cal").innerHTML = caleedar(vear)
-function setCaleender(year){
-
-}
->>>>>>> 734a5c30aca7b105eff9793fc70e8ed4acad5ede
